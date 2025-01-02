84703e48117d2ddfaec850286b186fb98d78f0e3
imaolabiyi200@gmail.com <Ima-obong Akinwumi Olabiyi>
2025-01-02 16:40:08 +0100

    node dependencies for server and payment method

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
-// import {SnackbarProvider} from 'notistack'
+import * as serviceWorker from "./serviceWorker";
+import {SnackbarProvider} from 'notistack'
 ReactDOM.render(
   <React.StrictMode>
 <SnackbarProvider maxSnack={3}> 
--- server/models/users.js
+++ server/models/users.js
@@ -1,25 +1,49 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
-    cName: {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      index: { unique: true },
+      match: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+    },
+    password: {
       type: String,
       required: true,
     },
-    cDescription: {
-      type: String,
+    userRole: {
+      type: Number,
       required: true,
     },
-    cImage: {
-      type: String,
+    phoneNumber: {
+      type: Number,
     },
-    cStatus: {
+    userImage: {
       type: String,
-      required: true,
+      default: "user.png",
+    },
+    verified: {
+      type: String,
+      default: false,
+    },
+    secretKey: {
+      type: String,
+      default: null,
+    },
+    history: {
+      type: Array,
+      default: [],
     },
   },
   { timestamps: true }
 );
 
-const categoryModel = mongoose.model("categories", categorySchema);
-module.exports = categoryModel;
+const userModel = mongoose.model("users", userSchema);
+module.exports = userModel;
