4a569f6d994d6d138dbebc8f0281b35336dda84e
imaolabiyi200@gmail.com <Ima-obong Akinwumi Olabiyi>
2025-01-03 05:41:49 +0100

    port change

--- server/app.js
+++ server/app.js
@@ -52,7 +52,7 @@ app.use("/api/order", orderRouter);
 app.use("/api/customize", customizeRouter);
 
 // Run Server
-const PORT = process.env.PORT || 8000;
+const PORT = "https://inquisitive-sable-47febc.netlify.app/";
 app.listen(PORT, () => {
   console.log("Server is running on ", PORT);
 });
